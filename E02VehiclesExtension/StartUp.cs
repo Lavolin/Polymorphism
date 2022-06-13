11ca32f3daff7e4c2b040eb70a94009db5e6ed8b
Lavolin <lavolin@gmail.com>
2022-06-13 15:14:03 +0300

    13061513

---
+++ E02VehiclesExtension/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace E02VehiclesExtension
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine().Split();
+            string[] truckInfo = Console.ReadLine().Split();
+
+            double carFuelQty = double.Parse(carInfo[1]);
+            double carLtPerKm = double.Parse(carInfo[2]);
+
+            double truckFuelQty = double.Parse(truckInfo[1]);
+            double truckLtPerKm = double.Parse(truckInfo[2]);
+
+            Vehicle car = new Car(carFuelQty, carLtPerKm);
+            Vehicle truck = new Truck(truckFuelQty, truckLtPerKm);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmd = Console.ReadLine().Split();
+                string typeOfCommand = cmd[0];
+                string typeOfVehicle = cmd[1];
+                double value = double.Parse(cmd[2]);
+
+                Vehicle vehicle = null;
+                if (typeOfVehicle == "Truck")
+                {
+                    vehicle = truck;
+                }
+                else
+                {
+                    vehicle = car;
+                }
+
+                if (typeOfCommand == "Drive")
+                {
+                    if (vehicle.CanDrive(value))
+                    {
+                        vehicle.Drive(value);
+                        Console.WriteLine($"{typeOfVehicle} travelled {value} km");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{typeOfVehicle} needs refueling");
+                    }
+                }
+                else
+                {
+
+                    vehicle.Refuel(value);
+                }
+
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:f2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:f2}");
+        }
+    }
+}
---
+++ E02VehiclesExtension/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace E02VehiclesExtension
+{
+    public abstract class Vehicle
+    {
+        private double fuelQuantity;
+
+        protected Vehicle(double tankCapacity, double fuelQuantity, double fuelConsumptionPerKm)
+        {
+            this.TankCapacity = tankCapacity;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+        }
+
+        public double FuelQuantity
+        {
+            get => fuelQuantity;
+            private set
+            {
+                if (value > this.TankCapacity)
+                {
+                    fuelQuantity = 0;
+                }
+                else
+                {
+
+                    fuelQuantity = value;
+                }
+            }
+        }
+        public virtual double FuelConsumptionPerKm { get; protected set; }
+
+        public double TankCapacity { get; }
+
+
+        public bool CanDrive(double km)
+            => this.FuelQuantity - (this.FuelConsumptionPerKm * km) >= 0;
+
+        public bool CanRefuel(double amount)
+             => this.FuelQuantity + amount <= this.TankCapacity;
+        public void Drive(double km)
+        {
+            if (CanDrive(km))
+            {
+                this.FuelQuantity -= (this.FuelConsumptionPerKm * km);
+            }
+        }
+
+        public virtual void Refuel(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+            if (CanRefuel(amount))
+            {
+                this.FuelQuantity += amount;
+
+            }
+        }
+    }
+}
